5ea6d9f6cbfdba863fa0e78217d05660e14d0d25
Igor Costiuc <gast777@yahoo.com>
2022-03-13 23:33:48 +0300

    adding taskbar

--- public/script/script.js
+++ public/script/script.js
@@ -1,3 +1,4 @@
+// Clock
 const loadTime = () => {
     const today = new Date();
     let h = today.getHours();
@@ -14,4 +15,30 @@ const checkTime = (i) => {
         i = "0" + i;
     }
     return i;
+}
+// toDo list
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#taskcreate input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#taskcreate input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        let current_tasks = document.querySelectorAll(".delete");
+        for(let i = 0; i < current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
 }
