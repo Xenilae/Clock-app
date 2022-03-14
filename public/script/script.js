ec5fcd474433701ef733ce8f35cbdc3fcd1b84d5
Igor Costiuc <gast777@yahoo.com>
2022-03-14 09:21:50 +0300

    messing up with styles

--- public/script/script.js
+++ public/script/script.js
@@ -30,7 +30,7 @@ document.querySelector('#push').onclick = function() {
                     ${document.querySelector('#taskcreate input').value}
                 </span>
                 <button class="delete">
-                    <i class="far fa-trash-alt"></i>
+                    <i class="far fa-trash-alt">Done</i>
                 </button>
             </div>
         `;
